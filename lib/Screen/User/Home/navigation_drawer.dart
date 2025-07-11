ac08ef72c7ee9a8a69f930ccf2f0ba2359a8c383
khanhmf <khanhmf@brothergroup.net>
2025-07-11 17:29:43 +0700

    5th

--- lib/Screen/User/Home/navigation_drawer.dart
+++ lib/Screen/User/Home/navigation_drawer.dart
@@ -10,133 +10,323 @@ class MenuScreen extends StatefulWidget {
 }
 
 class _MenuScreenState extends State<MenuScreen> {
+//  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: appBar(),
+      body: body(),
       drawer: ComplexDrawer(),
-      drawerScrimColor: Colors.blueGrey.withOpacity(.3),
-      backgroundColor: Common.grayColor.withOpacity(.5),
+      drawerScrimColor: Colors.transparent,
+      backgroundColor: Colors.white,
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      iconTheme: IconTheme.of(context).copyWith(
+        color:Colors.white,
+      ),
+      title: Text(
+        "LABOR CONTRACT EVALUATION",
+        style: TextStyle(
+        color: Colors.white,
+        ),
+      ),
+      backgroundColor: Common.primaryColor.withOpacity(0.6),
+    );
+  }
+
+  Widget body() {
+    return Center(
+      child: Container(
+        foregroundDecoration: BoxDecoration(
+          color: Common.primaryColor,
+          backgroundBlendMode: BlendMode.saturation,
+        ),
+        child: Container()
+        // FlutterLogo(
+        //   size: 150,
+        // ),
+      ),
     );
   }
 }
-Widget appBar(){
-  return AppBar(
-    title: 
-    Text('LABOR CONTRACT EVALUATION',style: TextStyle(
-        fontSize: 18,
-        fontWeight: FontWeight.bold,
-        color: Common.primaryColor,
-      ),
-    ),
-    backgroundColor: Common.greenColor.withOpacity(.3),
 
-  );
-}
 class ComplexDrawer extends StatefulWidget {
-  const ComplexDrawer({super.key});
+  const ComplexDrawer({Key? key}) : super(key: key);
 
   @override
-  State<ComplexDrawer> createState() => _ComplexDrawerState();
+  _ComplexDrawerState createState() => _ComplexDrawerState();
 }
 
 class _ComplexDrawerState extends State<ComplexDrawer> {
-  int selectedIndex = -1;
+  int selectedIndex = -1; //dont set it to 0
+
+  bool isExpanded = false;
+
   @override
   Widget build(BuildContext context) {
-    double widget = MediaQuery.of(context).size.width;
+    double width = MediaQuery.of(context).size.width;
     return Container(
-      width: widget/2,
+      // do dai menu
+      //width: width/2,
       child: row(),
+      // color: 
+      // Color.fromARGB(255, 119, 201, 146).withOpacity(0.2),
     );
   }
-  Widget row(){
-    return Row(
-      children: [
-        blackIconMenu(),
-        invisibleSubMenus(),
-      ],
+
+  Widget row() {
+    return Row(children: [
+      isExpanded ? blackIconTiles() : blackIconMenu(),
+      invisibleSubMenus(),
+    ]);
+  }
+
+  Widget blackIconTiles() {
+    return Container(
+      width: 300,
+      color: Common.primaryColor.withOpacity(0.6),
+      child: Column(
+        children: [
+          controlTile(),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cdms.length,
+              itemBuilder: (BuildContext context, int index) {
+                //  if(index==0) return controlTile();
+                CDM cdm = cdms[index];
+                bool selected = selectedIndex == index;
+                return ExpansionTile(
+                    onExpansionChanged: (z) {
+                      setState(() {
+                        selectedIndex = z ? index : -1;
+                      });
+                    },
+                    leading: Icon(cdm.icon, color: Colors.white),
+                    title: Text(
+                      cdm.title,
+                      style: TextStyle(
+                      color: Colors.white,
+                      ),
+                    ),
+                    trailing: cdm.submenus.isEmpty
+                        ? null
+                        : Icon(
+                            selected ? Icons.keyboard_arrow_up : Icons.keyboard_arrow_down,
+                            color: Colors.white,
+                          ),
+                    children: cdm.submenus.map((subMenu) {
+                      return sMenuButton(subMenu, false);
+                    }).toList());
+              },
+            ),
+          ),
+          accountTile(),
+        ],
+      ),
+    );
+  }
+
+  Widget controlTile() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Common.primaryColor,
+        borderRadius:  BorderRadius.only(topRight: Radius.circular(8))
+      ),
+      child: Padding(
+        padding: EdgeInsets.only(top: 20, bottom: 30),
+        child: ListTile(
+          leading: FlutterLogo(),
+          title: Text(
+            "LABOR CONTRACT EVALUATION",
+            style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+          ),
+          onTap: expandOrShrinkDrawer,
+        ),
+      ),
     );
   }
 
   Widget blackIconMenu() {
-      return Container(
-        width: 100,
-        color: Common.primaryColor,
-        child: ListView.builder(
-          itemCount: icons.length,
-          itemBuilder: (context , index){
-            return InkWell(
-              onTap: () {
-                setState((){
-                  selectedIndex = index;
-                });
-              },
-              child: Container(
-                height:  45,
-                alignment: Alignment.center,
-                child: Icon(icons[index].icon, color: Colors.white,),
-              ),
-            );
-          }
-        ),
-      );
-  }
-  Widget invisibleSubMenus() {
-      return Container(
-        width: 200,
-        color: Color.fromARGB(255, 119, 201, 146).withOpacity(0.2),
-        child: ListView.builder(
-          itemCount: icons.length,
-          itemBuilder: (context , index){
-            CDM cmd = icons[index];
-            bool selected = selectedIndex == index;
-            bool isvaule = selected && cmd.submenus.isNotEmpty;
-            return subMenuWidget(cmd.title, cmd.submenus, isvaule);
-          }
-        ),
-      );
-  }
-  Widget subMenuWidget(String title, List<String> submenus, bool selected){
     return AnimatedContainer(
       duration: Duration(seconds: 1),
-      height: selected ? 200 : 45,
-      decoration: BoxDecoration(
-        color: selected ?  Colors.white60 : Colors.transparent,
-        borderRadius: BorderRadius.only(topRight: Radius.circular(8), bottomRight: Radius.circular(8))
-      ),
-      color: Common.primaryColor.withOpacity(.5),
-      child: ListView.builder(
-        padding: EdgeInsets.all(6),
-        itemCount: selected ? submenus.length : 0,
-        itemBuilder: (context, index){
-          String subMenu = submenus[index];
-          return InkWell(
-            onTap: () {
-              
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(subMenu, style: TextStyle(color: Common.primaryColor,fontWeight: FontWeight.bold),
-              ),
-            ),
-          );
-        }
+      width: 100,
+      color: Common.primaryColor.withOpacity(0.8),
+      child: Column(
+        children: [
+          controlButton(),
+          Expanded(
+            child: ListView.builder(
+                itemCount: cdms.length,
+                itemBuilder: (contex, index) {
+                  // if(index==0) return controlButton();
+                  return InkWell(
+                    onTap: () {
+                      setState(() {
+                        selectedIndex = index;
+                      });
+                    },
+                    child: Container(
+                      height: 45,
+                      alignment: Alignment.center,
+                      child: Icon(cdms[index].icon, color: Colors.white),
+                    ),
+                  );
+                }),
+          ),
+          accountButton(),
+        ],
       ),
     );
   }
-  Widget isSubMenuWidget(){
-    return Container(
-      height:  45,
-      color: Colors.green,
+
+  Widget invisibleSubMenus() {
+    // List<CDM> _cmds = cdms..removeAt(0);
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 500),
+      width: isExpanded ? 0 : 200,
+      color:  Color.fromARGB(255, 119, 201, 146).withOpacity(0.4),
+      child: Column(
+        children: [
+          Container(height: 95),
+          Expanded(
+            child: ListView.builder(
+                itemCount: cdms.length,
+                itemBuilder: (context, index) {
+                  CDM cmd = cdms[index];
+                  // if(index==0) return Container(height:95);
+                  //controll button has 45 h + 20 top + 30 bottom = 95
+
+                  bool selected = selectedIndex == index;
+                  bool isValidSubMenu = selected && cmd.submenus.isNotEmpty;
+                  return subMenuWidget([cmd.title]..addAll(cmd.submenus), isValidSubMenu);
+                }),
+          ),
+        ],
+      ),
     );
+  }
+
+  Widget controlButton() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20, bottom: 30),
+      child: InkWell(
+        onTap: expandOrShrinkDrawer,
+        child: Container(
+          height: 45,
+          alignment: Alignment.center,
+          child: FlutterLogo(
+            size: 40,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget subMenuWidget(List<String> submenus, bool isValidSubMenu) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 500),
+      height: isValidSubMenu ? submenus.length.toDouble() * 70 : 45,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: isValidSubMenu ? Common.primaryColor.withOpacity(0.7) : Colors.transparent,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(8),
+            bottomRight: Radius.circular(8),
+          )),
+      child: ListView.builder(
+          padding: EdgeInsets.all(6),
+          itemCount: isValidSubMenu ? submenus.length : 0,
+          itemBuilder: (context, index) {
+            String subMenu = submenus[index];
+            return sMenuButton(subMenu, index == 0);
+          }),
+    );
+  }
+
+  Widget sMenuButton(String subMenu, bool isTitle) {
+    return InkWell(
+      onTap: () {
+        //handle the function
+        //if index==0? donothing: doyourlogic here
+      },
+      child: Container(
+        width: 200,
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          subMenu,
+          style: TextStyle(
+          fontSize: isTitle ? 17 : 14,
+          color: isTitle ? Colors.white : Colors.white70,
+          fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.left,
+        ),
+      ),
+    );
+  }
+
+  Widget accountButton({bool usePadding = true}) {
+    return Padding(
+      padding: EdgeInsets.all(usePadding ? 8 : 0),
+      child: AnimatedContainer(
+        duration: Duration(seconds: 1),
+        height: 45,
+        width: 45,
+        decoration: BoxDecoration(
+          color: Colors.white70,
+          image: DecorationImage(
+            //ảnh 
+            image: AssetImage('assets/img/profile.jpg'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.circular(6),
+        ),
+      ),
+    );
+  }
+
+  Widget accountTile() {
+    return Container(
+      color: Common.primaryColor,
+      child: ListTile(
+        leading: accountButton(usePadding: false),
+        title: Text(
+          "Welocome",
+          style: TextStyle(
+            color: Colors.white,
+        ),
+        ),
+        subtitle: Text(
+          "Nguyễn Duy Khánh",
+          style: TextStyle(
+            color: Colors.white70,
+        ),
+        ),
+      ),
+    );
+  }
+
+  static List<CDM> cdms = [
+    CDM(Icons.grid_view, "Master", ["Quản lý User","Thông tin PTHC","Danh sách màn hình"]),
+    CDM(Icons.subscriptions, "Lập đánh giá", ["Hợp đồng thử nghề & học việc", "Hợp đồng không xác định thời gian"]),
+    CDM(Icons.explore, "Điền đánh giá", ["Hợp đồng thử nghề & học việc", "Hợp đồng không xác định thời gian"]),
+    CDM(Icons.markunread_mailbox, "Phê duyệt", ["Hợp đồng thử nghề & học việc", "Hợp đồng không xác định thời gian"]),
+    CDM(Icons.pie_chart, "Báo cáo", ["Hợp đồng thử nghề & học việc", "Hợp đồng không xác định thời gian"]),
+    // CDM(Icons.trending_up, "Chart", []),
+    // CDM(Icons.power, "Plugins", []),
+    CDM(Icons.settings, "Setting", []),
+  ];
+
+  void expandOrShrinkDrawer() {
+    setState(() {
+      isExpanded = !isExpanded;
+    });
   }
 }
-List<CDM> icons = [
-  CDM(Icons.grid_view, "Dashboard", []),
-  CDM(Icons.subscriptions, "Dashboard1", []),
-  CDM(Icons.markunread_mailbox, "Dashboard2", []),
-  CDM(Icons.pie_chart, "Dashboard3", ["Vuong","Tron","Tam giac"]),
-  CDM(Icons.trending_down, "Dashboard4", []),
-  CDM(Icons.power, "Dashboard5", []),
-];
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:web_labor_contract/Screen/User/Home/home_screen.dart';
 import 'package:web_labor_contract/Screen/User/Home/navigation_drawer.dart';
 import 'package:web_labor_contract/Screen/User/LoginScreen/login_screen.dart';
 
